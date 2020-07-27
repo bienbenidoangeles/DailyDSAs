e84522f3b430543c2db55c8a5f5a3f225bbb1752
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-07-27 07:38:37 -0400

    added Strings

---
+++ Strings/Are the letters unique?.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// Write a function that accepts a String as its only parameter, and returns true if the string has only unique letters, taking letter case into account.
+
+func challenge1(input: String) -> Bool {
+    let lowerCasedInput = input.lowercased()
+    
+    var uniqueInput = Set(lowerCasedInput)
+    for char in lowerCasedInput {
+        if uniqueInput.contains(char) {
+            uniqueInput.remove(char)
+        }
+    }
+    if uniqueInput.isEmpty {
+        return true
+    } else {
+        return false
+    }
+}
+
+assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
